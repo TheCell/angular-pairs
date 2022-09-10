e861e8165bf76c79ce62be1c54dc318efb124029
TheCell <simon_hischier@hotmail.com>
2022-09-10 15:15:39 +0200

    stricter checking and preparation

--- angular-pairs-client/src/app/app.module.ts
+++ angular-pairs-client/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GameScreenComponent } from './game-screen/game-screen.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GameScreenComponent
   ],
   imports: [
     BrowserModule,
